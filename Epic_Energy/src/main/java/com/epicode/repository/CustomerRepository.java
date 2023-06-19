568f301c605e1cd06440339708c6226357ffee71
andrea-ragalzi <andrea.ragalzi.code@gmail.com>
2023-06-19 18:02:17 +0200

    removed unused imports

--- Epic_Energy/src/main/java/com/epicode/repository/CustomerRepository.java
+++ Epic_Energy/src/main/java/com/epicode/repository/CustomerRepository.java
@@ -1,11 +1,11 @@
 package com.epicode.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+
 
 import com.epicode.models.Customer;
 
-@Repository
+
 public interface CustomerRepository extends JpaRepository<Customer, String>{
 
 }
--- Epic_Energy/src/main/java/com/epicode/repository/InvoiceRepository.java
+++ Epic_Energy/src/main/java/com/epicode/repository/InvoiceRepository.java
@@ -1,11 +1,11 @@
 package com.epicode.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+
 
 import com.epicode.models.Invoice;
 
-@Repository
+
 public interface InvoiceRepository extends JpaRepository<Invoice, Long>{
 
 }
