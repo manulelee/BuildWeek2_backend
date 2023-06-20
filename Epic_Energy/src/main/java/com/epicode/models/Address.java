28f262bb766c3b3f3dffaa1ca99d13a2cd68461f
andrea-ragalzi <andrea.ragalzi.code@gmail.com>
2023-06-20 17:57:39 +0200

    added constructor with no id

--- Epic_Energy/src/main/java/com/epicode/models/Address.java
+++ Epic_Energy/src/main/java/com/epicode/models/Address.java
@@ -1,6 +1,5 @@
 package com.epicode.models;
 
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -14,29 +13,39 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name="addresses")
+@Table(name = "addresses")
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 
 public class Address {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	@Column(nullable = false)
 	private String street;
-	
+
 	@Column(nullable = false)
 	private Integer number;
-	
+
 	private String locality;
-	
+
 	@Column(name = "zip_code", nullable = false)
 	private Integer zipCode;
-	
+
 	@JoinColumn(nullable = false)
 	@ManyToOne
 	private City city;
+
+	public Address(
+			String street, Integer number, String locality,
+			Integer zipCode, City city) {
+		this.street = street;
+		this.number = number;
+		this.locality = locality;
+		this.zipCode = zipCode;
+		this.city = city;
+	}
 }
