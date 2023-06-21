f2b099b3b042f48fb50970f03d67842e5eba6387
Sara-Cb <saramaiden23@gmail.com>
2023-06-21 23:42:28 +0200

    added specifications, must test filters

---
+++ Epic_Energy/src/main/java/com/epicode/specifications/CustomerSpecifications.java
@@ -0,0 +1,30 @@
+package com.epicode.specifications;
+
+import java.time.LocalDate;
+
+import org.springframework.data.jpa.domain.Specification;
+
+import com.epicode.models.Customer;
+
+public class CustomerSpecifications {
+
+    public static Specification<Customer> containsLegalName(String name) {
+        return (root, query, builder) ->
+                builder.like(builder.lower(root.get("legalName")), "%" + name.toLowerCase() + "%");
+    }
+
+    public static Specification<Customer> annualIncomeBetween(Double minIncome, Double maxIncome) {
+        return (root, query, builder) ->
+                builder.between(root.get("annualIncome"), minIncome, maxIncome);
+    }
+
+    public static Specification<Customer> registrationDateBetween(LocalDate minDate, LocalDate maxDate) {
+        return (root, query, builder) ->
+                builder.between(root.get("registrationDate"), minDate, maxDate);
+    }
+
+    public static Specification<Customer> lastContactDateBetween(LocalDate minDate, LocalDate maxDate) {
+        return (root, query, builder) ->
+                builder.between(root.get("lastContactDate"), minDate, maxDate);
+    }
+}
