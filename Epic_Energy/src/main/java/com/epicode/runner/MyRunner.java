8cc56a8385c2258c0bd12ccaa158c4004009632a
andrea-ragalzi <andrea.ragalzi.code@gmail.com>
2023-06-21 16:13:12 +0200

    check files csv existence

--- Epic_Energy/src/main/java/com/epicode/runner/MyRunner.java
+++ Epic_Energy/src/main/java/com/epicode/runner/MyRunner.java
@@ -1,6 +1,7 @@
 package com.epicode.runner;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.time.LocalDate;
 import java.util.List;
@@ -37,7 +38,7 @@ public class MyRunner implements ApplicationRunner {
 
 	@Autowired
 	AddressService addressService;
-	
+
 	@Autowired
 	CustomerService customerService;
 
@@ -56,8 +57,14 @@ public class MyRunner implements ApplicationRunner {
 		if (!provinceService.getAllProvinces().isEmpty()) {
 			return;
 		}
-		//String file = "src/main/resources/province-italiane.csv";
-		String file = "src/main/resources/province-italiane.csv";
+		String filePath = "src/main/resources/province-italiane.csv";
+		File file = new File(filePath);
+		if (file.exists() && !file.isDirectory()) {
+			System.out.println("File exists");
+		} else {
+			filePath = "Epic_Energy/src/main/resources/province-italiane.csv";
+			file = new File(filePath);
+		}
 		String line;
 		try (
 				BufferedReader br = new BufferedReader(new FileReader(file))) {
@@ -77,11 +84,20 @@ public class MyRunner implements ApplicationRunner {
 		if (!cityService.getAllCities().isEmpty()) {
 			return;
 		}
-		//String file = "src/main/resources/comuni-italiani.csv";
-		String file = "src/main/resources/comuni-italiani.csv";
+		// java.io.FileNotFoundException
+		// String file = "src/main/resources/comuni-italiani.csv";
+		String filePath = "src/main/resources/comuni-italiani.csv";
+		File file = new File(filePath);
+		if (file.exists() && !file.isDirectory()) {
+			System.out.println("File exists");
+		} else {
+			filePath = "Epic_Energy/src/main/resources/comuni-italiani.csv";
+			file = new File(filePath);
+		}
+		;
 		String line;
 		try (
-				BufferedReader br = new BufferedReader(new FileReader(file))) {
+				BufferedReader br = new BufferedReader(new FileReader(filePath))) {
 			br.readLine();
 			while ((line = br.readLine()) != null) {
 				String[] fields = line.split(";");
@@ -97,51 +113,53 @@ public class MyRunner implements ApplicationRunner {
 	public Address insertAddress() {
 		Faker faker = new Faker(new Locale("it", "IT"));
 		List<City> cities = this.cityService.getAllCities();
-	//	for (int i = 0; i < 10; i++) {
-			String street = faker.address().streetName();
-			System.out.println(street);
-			Integer number = Integer.parseInt(faker.address().buildingNumber());
-			System.out.println(number);
-			String locality = faker.lorem().words(1).get(0);
-			Integer zipCode = Integer.parseInt(
-					faker.address().zipCode());
-			System.out.println(zipCode);
-			City city = cities.get(
-					faker.number().numberBetween(0, cities.size() - 1));
-			System.out.println(city);
-			Address address = new Address(
-					street, number, locality, zipCode, city);
-			System.out.println(address);
-			return address;
-	//		addressService.createAddress(address);
-	//	}
+		// for (int i = 0; i < 10; i++) {
+		String street = faker.address().streetName();
+		System.out.println(street);
+		Integer number = Integer.parseInt(faker.address().buildingNumber());
+		System.out.println(number);
+		String locality = faker.lorem().words(1).get(0);
+		Integer zipCode = Integer.parseInt(
+				faker.address().zipCode());
+		System.out.println(zipCode);
+		City city = cities.get(
+				faker.number().numberBetween(0, cities.size() - 1));
+		System.out.println(city);
+		Address address = new Address(
+				street, number, locality, zipCode, city);
+		System.out.println(address);
+		return address;
+		// addressService.createAddress(address);
+		// }
 	}
-	
+
 	public String randomVatNumber() {
 		Faker faker = new Faker(new Locale("it", "IT"));
 		String vat = "";
 		for (int i = 0; i < 11; i++) {
-			vat += faker.number().numberBetween(0, 9);;
+			vat += faker.number().numberBetween(0, 9);
+			;
 		}
 		return vat;
 	}
-	
-	public void insertCustomer () {
+
+	public void insertCustomer() {
 		if (!customerService.getAllCustomers().isEmpty()) {
 			return;
 		}
 		Faker faker = new Faker(new Locale("it", "IT"));
 		for (int i = 0; i < 10; i++) {
 			String vat = randomVatNumber();
-			String legalName = faker.company().name(); //da modificare
-			String email = "amministrazione"+ "@" +legalName.split(" ")[0].split(",")[0].toLowerCase() + ".com";
+			String legalName = faker.company().name(); // da modificare
+			String email = "amministrazione" + "@" + legalName.split(" ")[0].split(",")[0].toLowerCase() + ".com";
 			String pec = legalName.split(" ")[0].split(",")[0].toLowerCase() + "@pec.com";
 			String phone = faker.phoneNumber().phoneNumber();
 			String contactName = faker.name().firstName();
 			String contactLastName = faker.name().lastName();
-			String contactEmail = contactName.toLowerCase() + "." + contactLastName.toLowerCase() + "@" + legalName.split(" ")[0].split(",")[0].toLowerCase() + ".com";
+			String contactEmail = contactName.toLowerCase() + "." + contactLastName.toLowerCase() + "@"
+					+ legalName.split(" ")[0].split(",")[0].toLowerCase() + ".com";
 			String contactPhone = faker.phoneNumber().cellPhone();
-			
+
 			Customer customer = new Customer();
 			customer.setVatNumber(vat);
 			customer.setLegalName(legalName);
@@ -157,13 +175,13 @@ public class MyRunner implements ApplicationRunner {
 			customer.setCategory(ClientType.SRL);
 			customer.getAddress().put(AddressType.OPERATIVE_SEAT, insertAddress());
 			customer.getAddress().put(AddressType.LEGAL_SEAT, insertAddress());
-			
+
 			System.out.println(customer);
 			customerService.createCustomer(customer);
-		
+
 		}
 	}
-	
+
 	public void insertInvoice() {
 		if (!invoiceService.getAllInvoices().isEmpty()) {
 			return;
