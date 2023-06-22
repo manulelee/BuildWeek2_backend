f55a00a17d457c304d5874256678403187ba6006
andrea-ragalzi <andrea.ragalzi.code@gmail.com>
2023-06-22 12:21:39 +0200

    nothing

--- Epic_Energy/src/main/java/com/epicode/repository/CustomerRepository.java
+++ Epic_Energy/src/main/java/com/epicode/repository/CustomerRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.epicode.models.Customer;
 
-public interface CustomerRepository extends JpaRepository<Customer, String>{
-
+public interface CustomerRepository extends JpaRepository<Customer, String> {
+    
 }
