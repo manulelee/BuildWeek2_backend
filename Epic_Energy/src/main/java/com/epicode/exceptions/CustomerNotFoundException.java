84cf61cc35992377240b894e4b7b6338c1283578
andrea-ragalzi <andrea.ragalzi.code@gmail.com>
2023-06-22 12:21:04 +0200

    created

---
+++ Epic_Energy/src/main/java/com/epicode/exceptions/CustomerNotFoundException.java
@@ -0,0 +1,13 @@
+package com.epicode.exceptions;
+
+public class CustomerNotFoundException extends RuntimeException {
+
+    public CustomerNotFoundException(String message) {
+        super(message);
+    }
+
+    public CustomerNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
