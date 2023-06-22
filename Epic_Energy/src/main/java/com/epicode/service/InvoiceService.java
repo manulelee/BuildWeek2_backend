b7d2fbbc980409de90ab1f25394439539efec73d
andrea-ragalzi <andrea.ragalzi.code@gmail.com>
2023-06-22 17:02:39 +0200

    added InvoicePageRepository

--- Epic_Energy/src/main/java/com/epicode/service/InvoiceService.java
+++ Epic_Energy/src/main/java/com/epicode/service/InvoiceService.java
@@ -17,68 +17,69 @@ import com.epicode.repository.InvoicePageRepository;
 @Service
 public class InvoiceService {
 
-	@Autowired private InvoiceRepository repository;
-	@Autowired private InvoicePageRepository repoPage;
-	
-	public List<Invoice> getAllInvoices(){
+	@Autowired
+	private InvoiceRepository repository;
+	@Autowired
+	private InvoicePageRepository repoPage;
+
+	public List<Invoice> getAllInvoices() {
 		return repository.findAll();
 	}
-	
-	public Page<Invoice> getAllInvoicesPage(Pageable pageable){
+
+	public Page<Invoice> getAllInvoicesPage(Pageable pageable) {
 		return repoPage.findAll(pageable);
 	}
-	
-	public Invoice getInvoiceById (Long id) {
+
+	public Invoice getInvoiceById(Long id) {
 		if (!repository.existsById(id)) {
-			//gestione eccezione
+			// gestione eccezione
 		}
 		return repository.findById(id).get();
 	}
-	
+
 	public Invoice createInvoice(Invoice invoice) {
 		System.out.print("invoice saved");
 		return repository.save(invoice);
 	}
-	
-	public Invoice updateInvoice (Long id, Invoice invoice) {
+
+	public Invoice updateInvoice(Long id, Invoice invoice) {
 		if (!repository.existsById(id)) {
-			//gestione eccezione
+			// gestione eccezione
 		}
 		return repository.save(invoice);
 	}
-	
-	public String removeInvoice (Long id) {
+
+	public String removeInvoice(Long id) {
 		if (!repository.existsById(id)) {
-			//gestione eccezione
+			// gestione eccezione
 		}
 		repository.deleteById(id);
 		return "Invoice removed";
 	}
-	
-	
-	//Queries
+
+	// Queries
 	public Page<Invoice> getInvoicesByCustomer(Customer customer, Pageable pageable) {
-        return repository.findByCustomer(customer, pageable);
-    }
+		return repoPage.findByCustomer(customer, pageable);
+	}
 
-    public Page<Invoice> getInvoicesByState(InvoiceState state, Pageable pageable) {
-        return repository.findByState(state, pageable);
-    }
+	public Page<Invoice> getInvoicesByState(InvoiceState state, Pageable pageable) {
+		return repoPage.findByState(state, pageable);
+	}
 
-    public Page<Invoice> getInvoicesByDate(LocalDate date, Pageable pageable) {
-        return repository.findByDate(date, pageable);
-    }
+	public Page<Invoice> getInvoicesByDate(LocalDate date, Pageable pageable) {
+		return repoPage.findByDate(date, pageable);
+	}
 
-    public Page<Invoice> getInvoicesByDateRange(LocalDate startDate, LocalDate endDate, Pageable pageable) {
-        return repository.findByDateBetween(startDate, endDate, pageable);
-    }
+	public Page<Invoice> getInvoicesByDateRange(LocalDate startDate, LocalDate endDate, Pageable pageable) {
+		return repoPage.findByDateBetween(startDate, endDate, pageable);
+	}
 
-    public Page<Invoice> getInvoicesByYear(int year, Pageable pageable) {
-        return repository.findByYear(year, pageable);
-    }
+	public Page<Invoice> getInvoicesByYear(int year, Pageable pageable) {
+		return repoPage.findByYear(year, pageable);
+	}
 
-    public Page<Invoice> getInvoicesByAmountRange(Double minAmount, Double maxAmount, Pageable pageable) {
-        return repository.findByAmountBetween(minAmount, maxAmount, pageable);
-    }
-	
+	public Page<Invoice> getInvoicesByAmountRange(Double minAmount, Double maxAmount, Pageable pageable) {
+		return repoPage.findByAmountBetween(minAmount, maxAmount, pageable);
+	}
+
 }
