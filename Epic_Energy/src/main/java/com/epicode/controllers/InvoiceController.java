d05140670b597beb5b17fe60e540f2416aa41316
andrea-ragalzi <andrea.ragalzi.code@gmail.com>
2023-06-23 12:09:24 +0200

    add comments

--- Epic_Energy/src/main/java/com/epicode/controllers/InvoiceController.java
+++ Epic_Energy/src/main/java/com/epicode/controllers/InvoiceController.java
@@ -51,8 +51,8 @@ public class InvoiceController {
 	@GetMapping("/all")
 	@ResponseBody
 	@PreAuthorize("isAuthenticated()")
-	public ResponseEntity<List<Invoice>> getAllInvoices (){
-		return new ResponseEntity<List<Invoice>>(service.getAllInvoices(),HttpStatus.OK); 	
+	public ResponseEntity<List<Invoice>> getAllInvoices() {
+		return new ResponseEntity<List<Invoice>>(service.getAllInvoices(), HttpStatus.OK);
 	}
 
 	@GetMapping
@@ -107,81 +107,106 @@ public class InvoiceController {
 		}
 	}
 
-	// Exception handler for any other exceptions not handled by the above methods
-	@ExceptionHandler(Exception.class)
-	public ResponseEntity<String> handleException(Exception e) {
-		logger.error("An error occurred: {}", e.getMessage());
-		return new ResponseEntity<String>(
-				e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-	}
-
 	/**
-	 * Filters invoices based on the given parameters and returns a page of invoices that match the criteria.
+	 * Filters invoices based on the given parameters and returns a page of invoices
+	 * that match the criteria.
 	 *
-	 * @param  customer    the ID of the customer to filter by, if provided
-	 * @param  state       the state of the invoices to filter by, if provided
-	 * @param  date        the date of the invoices to filter by, if provided
-	 * @param  year        the year of the invoices to filter by, if provided
-	 * @param  minAmount   the minimum amount of the invoices to filter by, if provided
-	 * @param  maxAmount   the maximum amount of the invoices to filter by, if provided
-	 * @param  pageable    the paging and sorting information
-	 * @return             a page of invoices that match the provided criteria
+	 * @param customer  the ID of the customer to filter by, if provided
+	 * @param state     the state of the invoices to filter by, if provided
+	 * @param date      the date of the invoices to filter by, if provided
+	 * @param year      the year of the invoices to filter by, if provided
+	 * @param minAmount the minimum amount of the invoices to filter by, if provided
+	 * @param maxAmount the maximum amount of the invoices to filter by, if provided
+	 * @param pageable  the paging and sorting information
+	 * @return a page of invoices that match the provided criteria
 	 */
 	@GetMapping("/filter")
 	@ResponseBody
 	@PreAuthorize("isAuthenticated()")
 	public Page<Invoice> filterInvoices(
+			// This annotation indicates that the parameter is a query parameter.
+			// The "required = false" attribute indicates that the parameter is optional.
 			@RequestParam(required = false) String customer,
 			@RequestParam(required = false) InvoiceState state,
 			@RequestParam(required = false) LocalDate date,
 			@RequestParam(required = false) Integer year,
 			@RequestParam(required = false) Double minAmount,
 			@RequestParam(required = false) Double maxAmount,
+			// This parameter specifies the page size, page number, and sorting of the
+			// result.
 			Pageable pageable) {
 
+		// If no query parameters are provided, return a page of all invoices.
 		if (customer == null && state == null && date == null &&
 				year == null && minAmount == null && maxAmount == null) {
 			return service.getAllInvoicesPage(pageable);
 		}
 
+		// This list will store the partial results for each query parameter that is
+		// provided.
 		List<Page<Invoice>> partialResults = new ArrayList<>();
 
+		// If the "customer" query parameter is provided, filter invoices by customer.
 		if (customer != null) {
 			Customer customerObj = customerService.getCustomerById(customer);
 			partialResults.add(service.getInvoicesByCustomer(customerObj, pageable));
 		}
 
+		// If the "state" query parameter is provided, filter invoices by state.
 		if (state != null) {
 			partialResults.add(service.getInvoicesByState(state, pageable));
 		}
 
+		// If the "date" query parameter is provided, filter invoices by date.
 		if (date != null) {
 			partialResults.add(service.getInvoicesByDate(date, pageable));
 		}
 
+		// If the "year" query parameter is provided, filter invoices by year.
 		if (year != null) {
 			partialResults.add(service.getInvoicesByYear(year, pageable));
 		}
 
+		// If both "minAmount" and "maxAmount" query parameters are provided,
+		// filter invoices by amount range.
 		if (minAmount != null && maxAmount != null) {
 			partialResults.add(service.getInvoicesByAmountRange(minAmount, maxAmount, pageable));
 		}
 
+		// This stream operation finds the intersection of all the partial results.
 		List<Invoice> commonInvoices = partialResults.stream()
+				// Map each partial result to its content (a list of invoices).
 				.map(Page::getContent)
+				// Flatten the list of lists into a single list.
 				.flatMap(Collection::stream)
+				// Group the invoices in the list by identity and count the occurrences of each
+				// invoice.
 				.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+				// Keep only the entries whose count is equal to the number of partial results.
 				.entrySet().stream()
 				.filter(e -> e.getValue() == partialResults.size())
+				// Map the entries back to the corresponding invoices.
 				.map(Map.Entry::getKey)
+				// Collect the invoices into a list.
 				.collect(Collectors.toList());
 
+		// This section constructs a page of the common invoices based on the "pageable"
+		// parameter.
 		int pageSize = pageable.getPageSize();
 		int pageNumber = pageable.getPageNumber();
 		int startIndex = pageNumber * pageSize;
 		int endIndex = Math.min(startIndex + pageSize, commonInvoices.size());
 		List<Invoice> pageContent = commonInvoices.subList(startIndex, endIndex);
-		return new PageImpl<>(pageContent, pageable, commonInvoices.size());
+		Page<Invoice> page = new PageImpl<>(pageContent, pageable, commonInvoices.size());
+		return page;
+	}
+
+	// Exception handler for any other exceptions not handled by the above methods
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<String> handleException(Exception e) {
+		logger.error("An error occurred: {}", e.getMessage());
+		return new ResponseEntity<String>(
+				e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 
 }
